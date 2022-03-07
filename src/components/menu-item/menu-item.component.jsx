d6ab699a95f656303e217f13ac4c47099203b882
AmritSilwal <amritsilwal79@gmail.com>
2022-03-07 21:05:49 +0200

    Home page broken into component and Styles menu item.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,10 @@ import './menu-item.styles.scss';
 
 // below we are using destructuring {} for title which is almost similar to use props.title
 const MenuItem = ({title, imageUrl,size}) => (
-    <div style={{backgroundImage:`url(${imageUrl})`}} 
-              className={ `${size} menu-item` }>
+    <div className={ `${size} menu-item` }>
+      <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}} />
       <div className='content'>
-        <h1 className='title'>{title}</h1> 
+        <h1 className='title'>{title.toUpperCase()}</h1> 
         <span className='subtitle'>SHOP NOW</span>
       </div>
     </div>
